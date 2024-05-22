c2c7e87c342e09538cd0d520af98a35de33f6db7
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-05-22 15:42:42 +0530

    bug fix and more section added

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,23 @@ import { FloatingNav } from "@/components/ui/FloatingNavBar";
 import Grid from "@/components/Grid";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-      <FloatingNav navItems={[
-        { name: "Home", link: "/", icon: <FaHome />}
-        
-      ]}/>
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
       </div>
     </main>
   );
